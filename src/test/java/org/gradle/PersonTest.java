49ec7dc2c9b2795cfe3cdf8e725f98c78e177efb
Evan Wang <evan.wang@dianrong.com>
2014-11-05 18:17:31 +0800

    test

--- src/test/java/org/gradle/PersonTest.java
+++ src/test/java/org/gradle/PersonTest.java
@@ -7,6 +7,6 @@ public class PersonTest {
     @Test
     public void canConstructAPersonWithAName() {
         Person person = new Person("Larry");
-        assertEquals("Lary", person.getName());
+        assertEquals("Larry", person.getName());
     }
 }
